91ae6cb9388b2e8f49cbf4011124828f44959eb7
mediumfact7007 <amildfacta090@gmail.com>
2026-09-10 21:04:29 +0200

    Update widget test for v0.4

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,11 +3,11 @@ import 'package:flipradar/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  testWidgets('FlipRadar starts', (tester) async {
+  testWidgets('FlipRadar v0.4 starts', (tester) async {
     SharedPreferences.setMockInitialValues({});
     await tester.pumpWidget(const FlipRadarApp());
     await tester.pumpAndSettle();
     expect(find.text('FlipRadar'), findsOneWidget);
-    expect(find.text('Schnellstart'), findsOneWidget);
+    expect(find.text('Live Preise vergleichen'), findsOneWidget);
   });
 }
