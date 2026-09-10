5c26eb4ee48bd5f7edc3cb1d5c1a783e4cbb6294
mediumfact7007 <amildfacta090@gmail.com>
2026-09-11 01:17:03 +0200

    Update startup test for v0.7

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,13 +3,12 @@ import 'package:flipradar/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  testWidgets('FlipRadar v0.6 starts', (tester) async {
-    SharedPreferences.setMockInitialValues({
-      'onboarding_v06': true,
-    });
+  testWidgets('FlipRadar v0.7 starts with action-first home', (tester) async {
+    SharedPreferences.setMockInitialValues({});
     await tester.pumpWidget(const FlipRadarApp());
     await tester.pumpAndSettle();
     expect(find.text('FlipRadar'), findsOneWidget);
-    expect(find.text('Barcode scannen'), findsOneWidget);
+    expect(find.text('Ist das ein guter Deal?'), findsOneWidget);
+    expect(find.text('BARCODE SCANNEN'), findsOneWidget);
   });
 }
