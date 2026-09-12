0c78068f26b60d0279306e0c2ea9a037c434be63
mediumfact7007 <amildfacta090@gmail.com>
2026-09-12 14:06:29 +0200

    Switch app entry point to FlipRadar V0.13

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/widgets.dart';
 
-import 'v10_app.dart';
+import 'v13_app.dart';
 
 export 'v07.dart';
 export 'v09_app.dart';
 export 'v10_app.dart';
+export 'v13_app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const FlipRadarV10App());
+  runApp(const FlipRadarV13App());
 }
