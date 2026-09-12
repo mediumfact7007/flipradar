fbd39a58a04f194dc1864298542da03bc43f6d78
mediumfact7007 <amildfacta090@gmail.com>
2026-09-12 02:14:08 +0200

    Make FlipRadar V0.10 the default app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/widgets.dart';
 
-import 'v09_app.dart';
+import 'v10_app.dart';
 
 export 'v07.dart';
 export 'v09_app.dart';
+export 'v10_app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const FlipRadarFinalApp());
+  runApp(const FlipRadarV10App());
 }
