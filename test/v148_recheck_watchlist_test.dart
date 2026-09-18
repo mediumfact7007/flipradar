715f4a8f0549c26666a8837a699ea228ee94ad0b
mediumfact7007 <amildfacta090@gmail.com>
2026-09-19 00:07:49 +0200

    Exercise recheck sale commit through visible action

--- test/v148_recheck_watchlist_test.dart
+++ test/v148_recheck_watchlist_test.dart
@@ -97,7 +97,9 @@ void main() {
     final saleField = tester.widget<TextField>(saleFinder);
     expect(saleField.controller?.text, '350');
     await tester.enterText(saleFinder, '350');
-    saleField.onSubmitted!('350');
+    final commit = find.descendant(of: saleFinder, matching: find.byIcon(Icons.check_rounded));
+    expect(commit, findsOneWidget);
+    await tester.tap(commit);
     await tester.pump();
 
     final remember = find.byKey(const ValueKey('v147-remember-deal'));
